4944dc0abb99a849c2d2800053a234af7b3e4e1a
isa <isa>
2018-08-04 00:54:46 +0900

    だいたい把握。

--- LinqPractice/LinqPractice/Program.cs
+++ LinqPractice/LinqPractice/Program.cs
@@ -10,6 +10,27 @@ namespace LinqPractice
     {
         static void Main(string[] args)
         {
+            var linqTest = new[] { 3, 4, 3, 50, 34, 5 };
+            var classTest = new[]
+            {
+                new {age = 10, name = "taro"},
+                new {age = 4,  name = "hana"},
+                new {age = 2,  name = "pes"},
+                new {age = 34, name = "ken"}
+            };
+
+            // selectは値を操作する.
+            //var tmp = linqTest.OrderBy(_ => _).Select(_ => _ * 2);
+            var tmp = classTest
+                .Where(_ => _.age >= 9)
+                .OrderBy(_ => _.age);
+
+            foreach (var item in tmp)
+            {
+                Console.WriteLine(item.name);
+                
+            }
+            Console.ReadKey();
         }
     }
 }
